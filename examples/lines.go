78092d0307400c269fe6cb4bd87aca1e4b5ea551
kwinso <ruslan.sot70@gmail.com>
2023-02-05 23:23:47 +0300

    Add LineModuleShape.

--- examples/lines.go
+++ examples/lines.go
@@ -4,6 +4,7 @@ import (
 	"github.com/quickqr/gqr"
 	export "github.com/quickqr/gqr/export/image"
 	"github.com/quickqr/gqr/export/image/shapes"
+	"image"
 	"image/png"
 	"log"
 	"os"
@@ -18,13 +19,22 @@ func main() {
 		log.Fatal(e)
 	}
 
+	logoFile, _ := os.Open("./fox.png")
+	logo, _, _ := image.Decode(logoFile)
+
 	// Export QR code to image
 	img := export.
 		NewExporter(
 			export.WithBgColorHex("#1f1f1f"),
 
+			export.WithLogo(logo),
+			export.WithSpaceAroundLogo(),
+
 			export.WithFinderShape(shapes.RoundedFinderShape(0.3)),
 			export.WithModuleShape(shapes.LineModuleShape(0.5, false)),
+
+			export.WithImageSize(1024),
+			export.WithQuietZone(60),
 			export.WithModuleGap(0.3),
 
 			export.WithGradient(export.GradientDirectionLTR,
